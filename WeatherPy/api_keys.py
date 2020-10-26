853789c97a3b92fee0da9ecf9f67c3b45d94413c
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-10-25 20:29:12 -0600

    kernelfailingnonstopgit add .!

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "dd00c3f6b1bff1e30f23d26dccaf525b"
+weather_api_key = "bf13c78057f42259750c2dcb4e1dde31"
 
 # Google API Key
 g_key = "IzaSyC0-N9YcVQYAydzXOQyNCh8fskYcuItusU"
